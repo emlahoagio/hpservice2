327c3a780605eaac0a871e3fd813fbc25db97d82
quando <quandtm0610@gmail.com>
2025-03-10 20:22:28 +0700

    show pop

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const popupOverlay = document.getElementById("popupOverlay");
 
 // Sau 2 giây, hiển thị popup
 setTimeout(function () {
-    popupOverlay.style.display = "flex";
-}, 1000);
+    popupOverlay.classList.add("show");
+}, 500);
